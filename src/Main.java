9b4ed8b4f778fcd0662fd108c11076319bbe4626
DYH <1245687900@qq.com>
2021-11-18 17:15:52 +0800

    更改

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 import view.GameFrame;
-
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-
             GameFrame mainFrame = new GameFrame(800);
             mainFrame.setVisible(true);
         });
