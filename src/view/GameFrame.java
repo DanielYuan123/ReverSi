c3feac9e851442cb6ab5430183888db5ef5cf883
Daniel Yuan <colinss.daniell530@gmail.com>
2021-11-28 23:46:09 +0800

    完成restartButton

--- src/view/GameFrame.java
+++ src/view/GameFrame.java
@@ -3,6 +3,9 @@ package view;
 import controller.GameController;
 
 import javax.swing.*;
+import javax.xml.soap.SOAPPart;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GameFrame extends JFrame {
     public static GameController controller;
@@ -33,38 +36,28 @@ public class GameFrame extends JFrame {
         this.add(chessBoardPanel);
         this.add(statusPanel);
 
+        //new一个自制的事件监听；
+        MyBtnActionListener myBtnActionListener = new MyBtnActionListener(this);
 
         JButton restartBtn = new JButton("Restart");
         restartBtn.setSize(120, 50);
         restartBtn.setLocation((this.getWidth() - chessBoardPanel.getWidth()) / 2, (this.getHeight() + chessBoardPanel.getHeight()) / 2);
         add(restartBtn);
-        restartBtn.addActionListener(e -> {
-            System.out.println("click restart Btn");
-            //todo: restart button.
-            
-        });
+        restartBtn.addActionListener(myBtnActionListener);
 
         
         JButton loadGameBtn = new JButton("Load");
         loadGameBtn.setSize(120, 50);
         loadGameBtn.setLocation(restartBtn.getX()+restartBtn.getWidth()+30, restartBtn.getY());
         add(loadGameBtn);
-        loadGameBtn.addActionListener(e -> {
-            System.out.println("clicked Load Btn");
-            String filePath = JOptionPane.showInputDialog(this, "input the path here");
-            controller.readFileData(filePath);
-        });
+        loadGameBtn.addActionListener(myBtnActionListener);
         
         
         JButton saveGameBtn = new JButton("Save");
         saveGameBtn.setSize(120, 50);
         saveGameBtn.setLocation(loadGameBtn.getX()+restartBtn.getWidth()+30, restartBtn.getY());
         add(saveGameBtn);
-        saveGameBtn.addActionListener(e -> {
-            System.out.println("clicked Save Btn");
-            String filePath = JOptionPane.showInputDialog(this, "input the path here");
-            controller.writeDataToFile(filePath);
-        });
+        saveGameBtn.addActionListener(myBtnActionListener);
         
         
         JButton nightModeBtn= new JButton("Nightmode");
@@ -81,5 +74,36 @@ public class GameFrame extends JFrame {
         this.setVisible(true);
         this.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
         
+    }
+
+    //构建一个事件监听的内部类；
+    private class MyBtnActionListener implements ActionListener{
+
+        //设置GameFrame的成员变量；
+        private GameFrame gameFrame;
+
+        //重写构造器，使监听器可以接触当前类；
+        public MyBtnActionListener(GameFrame gameFrame) {
+            this.gameFrame = gameFrame;
+        }
+
+        /*
+        实现ActionListener方法，restart则使当前GameFrame不可见，并重新new一个GameFrame;除此之外，加入Save和Load的事件监听方法；
+         */
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(e.getActionCommand().equals("Restart")){
+                System.out.println("clicked Restart button.");
+                this.gameFrame.setVisible(false);
+                new GameFrame(800);
+            }else if(e.getActionCommand().equals("Load")){
+                System.out.println("clicked Load Btn");
+                String filePath = JOptionPane.showInputDialog(this, "input the path here");
+                controller.readFileData(filePath);
+            }else if(e.getActionCommand().equals("Save")){System.out.println("clicked Save Btn");
+                String filePath = JOptionPane.showInputDialog(this, "input the path here");
+                controller.writeDataToFile(filePath);
+            }
+        }
     }
 }
